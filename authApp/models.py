39be9d554ccbbe8a7eb2763681637847c03022d8
johnmuthua <caveomuthama@gmail.com>
2023-07-29 10:38:48 +0300

    Add token table

--- authApp/models.py
+++ authApp/models.py
@@ -8,3 +8,11 @@ class User(Base):
     username = Column(String(50), nullable=False, unique=True)
     email = Column(String(150), unique=True, nullable=False, indexed=True)
     password = Column(String(250), nullable=False)
+
+class TokenTable(Base):
+    __tablename__ = "token"
+    user_id = Column(Integer)
+    access_token = Column(String(500), primary_key=True)
+    refresh_token = Column(String(500), nullable=False)
+    status = Column(Boolean)
+    created_date = Column(DateTime, default=datetime.datetime.now)
