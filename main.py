b1aaea69e3d8580129aa3eac6051094642be9add
johnmuthua <caveomuthama@gmail.com>
2023-07-29 10:09:31 +0300

    Add: register new user

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ def get_session():
 
 app = FastAPI()
 
+@app.post('/register')
+def register_user(user:schemas.UserCreate, session:Session = Depends(get_session)):
+    existing_user = session.query(models.User).filter_by(email=user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already exists")
+    password_hash = get_hashed_password(user.password)
+    new_user = models.User(user=user.username, password=password_hash, email=user.email)
+    session.add(new_user)
+    session.commit()
+    session.refresh(new_user)
+
+    return {'msg':'user created successfully', 'payload': new_user}
