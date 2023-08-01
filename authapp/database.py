120d4bd35214c62bc5c045a58e343aa2e1de1e48
johnmuthua <caveomuthama@gmail.com>
2023-08-01 11:30:46 +0300

    Add docker compose file

--- authapp/database.py
+++ authapp/database.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 
-engine = create_engine("postgresql+psycopg2://postgres:mypassword@localhost:5432/auth2")
+engine = create_engine(database_url)
 Base = declarative_base()
 
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
