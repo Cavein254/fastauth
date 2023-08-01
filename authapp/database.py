3ff5ebb83381d3d527faeef1b5e8d3892dfe9cc9
johnmuthua <caveomuthama@gmail.com>
2023-08-01 10:49:41 +0300

    Add alembic connect changes

--- authapp/database.py
+++ authapp/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
@@ -7,8 +7,7 @@ import os
 load_dotenv()
 database_url = os.getenv("DATABASE_URL")
 
-DATABASE_URL = database_url
-engine = create_engine(DATABASE_URL)
-Base = declarative_base(engine)
+engine = create_engine("postgresql+psycopg2://postgres:mypassword@localhost:5432/auth2")
+Base = declarative_base()
 
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
